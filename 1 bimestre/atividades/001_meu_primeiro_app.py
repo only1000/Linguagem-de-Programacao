b1fa6e5005b3710fce3ffa6b365adcde3fbb109d
Josué <josue2catunda@gmail.com>
2024-08-13 21:16:14 -0300

    primeiro app e atividade 01

---
+++ 1 bimestre/atividades/001_meu_primeiro_app.py
@@ -0,0 +1,18 @@
+#Meu primeiro app
+
+#print("Olá, mundo!")
+
+#nome="Astúbal"
+
+
+nome=input("Digite o seu nome: ")
+
+sobrenome=input("Digite o seu sobrenome: ")
+
+idade=input("Digite a sua idade: ")
+
+print("O Seu nome: " + nome)
+
+print("O seu sobrenome: " + sobrenome)
+
+print("A sua idade: " + idade)
---
+++ 1 bimestre/atividades/atividade_01.py
@@ -0,0 +1,11 @@
+nome=input("Digite o seu nome: ")
+
+matricula=input("Digite o seu número de matricula: ")
+
+curso=input("Digite o seu curso matriculado: ")
+
+print("Nome: " + nome)
+
+print("Número de Matrícula: " + matricula)
+
+print("Curso matriculado: " + curso)
